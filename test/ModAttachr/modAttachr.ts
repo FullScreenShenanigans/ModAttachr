724b114bbcd79d50534503eba0e2a36bfdc88cbd
Connor Hadley <Connor Hadley>
2017-03-07 09:01:48 -0500

    removed improperly named file

---
+++ test/ModAttachr/modAttachr.ts
@@ -0,0 +1,36 @@
+import { ModAttachr } from "../../src/ModAttachr";
+
+import { mochaLoader } from "../main";
+
+import { EventNames } from "../../src/EventNames";
+
+import {
+    IMod
+} from "../../src/IModAttachr";
+
+mochaLoader.it("Creates a mod and fires the events within it", (): void => {
+    // Arrange
+    const eventNames: EventNames = new EventNames();
+    const dummyMod: IMod[] = [{
+        name: "Dummy Mod",
+        events: {
+            onModDisable: (): string => {
+                return "success";
+            },
+            onModEnable: (): string => {
+                return "success";
+            }
+        },
+        enabled: false
+    }];
+    const modAttachr: ModAttachr = new ModAttachr({
+        mods: dummyMod,
+        storeLocally: true,
+        transformModName: undefined,
+        eventNames: eventNames
+    });
+
+    // Assert
+    chai.expect(modAttachr.fireModEvent("onModEnable", "Dummy Mod")).to.be.equal("success");
+    chai.expect(modAttachr.fireModEvent("onModDisable", "Dummy Mod")).to.be.equal("success");
+});
